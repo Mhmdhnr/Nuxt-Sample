a90f52d059cf809402e47112582401cbad3d9532
Mhmd <mhmd.hnr@gmail.com>
2021-11-07 20:21:27 +0330

    heroku...

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   server: {
-    host: "0.0.0.0",
-    port: 80
+    // host: "0.0.0.0",
+    // port: 80
   },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
