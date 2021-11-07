11bf7518a4899e4f4a949ec9a748478591b2ecca
Mhmd <mhmd.hnr@gmail.com>
2021-11-07 19:14:34 +0330

    deploy...

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+    export default {
+        components: {},
+        data(){
+            return {
+
+            }
+        },
+        mounted() {
+            console.log("from Index")
+          this.$router.push("Home")
+        }
+    }
+</script>
