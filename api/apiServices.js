38597e95e01a328f4a8a728a06a2ad65e00c32bf
Mhmd <mhmd.hnr@gmail.com>
2021-11-07 19:13:12 +0330

    deploy...

--- api/apiServices.js
+++ api/apiServices.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://127.0.0.1:5000';
+const API_URL = 'http://flask-restful-nuxt.herokuapp.com';
 let axi = axios.create({
   headers: {
     'Content-Type' : 'application/json',
